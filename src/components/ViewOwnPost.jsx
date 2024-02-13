1c242d74f65e77e846eacf863b4a425d602fe7a7
ArunKuriakose15 <arunkp1515@gmail.com>
2024-02-13 15:41:34 +0530

    new changes

--- src/components/ViewOwnPost.jsx
+++ src/components/ViewOwnPost.jsx
@@ -5,20 +5,34 @@ import axios from 'axios'
 const ViewOwnPost = () => {
     const [input, setInput] = new useState(
         {
-            userId:sessionStorage.getItem("userId")
+            userId: sessionStorage.getItem("userId")
         }
     )
     const [output, setOutput] = useState([]);
 
     const readValues = () => {
         console.log(input)
-        axios.post("http://localhost:3001/api/ownpost/viewown", input).then((response) => {
+        axios.post("http://localhost:3001/api/post/viewown", input).then((response) => {
             setOutput(response.data)
             console.log(response.data)
 
         })
     }
     useEffect(() => { readValues() }, [])
+
+    const deletePost = (id) => {
+        let data = { "_id": id }
+        axios.post("http://localhost:3001/api/post/delete",data).then((response)=>{
+            if(response.data.status=="success"){
+                alert("Post deleted")
+                readValues()
+            }
+            else{
+                alert("Something went wrong ...")
+            }
+        })
+    }
+
     return (
         <div>
 
@@ -29,27 +43,28 @@ const ViewOwnPost = () => {
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
                         <div className="row g-3">
-                           {
-                            output.map(
-                                (value,index)=>{
-                                    return  <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            {
+                                output.map(
+                                    (value, index) => {
+                                        return <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
-                                    <div className="card">
-                                        <div className="card-header">Post
+                                            <div className="card">
+                                                <div className="card-header">Post
+                                                </div>
+                                                <div className="card-body">
+                                                    <blockquote className="blockquote mb-0">
+                                                        <p>{value.post}</p>
+                                                        <footer className="blockquote-footer"> </footer>
+                                                    </blockquote>
+                                                    <p className="card-text"><small>Posted on {value.postedDate}</small></p>
+                                                    <button className="btn btn-danger" onClick={() => { deletePost(value._id) }} >Delete</button>
+                                                </div>
+                                            </div>
+
                                         </div>
-                                        <div className="card-body">
-                                            <blockquote className="blockquote mb-0">
-                                                <p>{value.post}</p>
-                                                <footer className="blockquote-footer"> </footer>
-                                            </blockquote>
-                                            <p className="card-text"><small>Posted on {value.postedDate}</small></p>
-                                        </div>
-                                    </div>
-    
-                                </div>
-                                }
-                            )
-                           }
+                                    }
+                                )
+                            }
                         </div>
 
                     </div>
